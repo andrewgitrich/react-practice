15d701783f4b062da30a5c2a1c2fbbeddef25e69
andrew <andrew_rich@hotmail.com>
2016-11-30 13:45:43 -0500

    initial

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
-require('babel-polyfill');
+require('babel-polyfill');//this line was here when i created index.js, there was no existing file previously
 
+// js/components/index.js
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var PersonList = require('./components/person-list');
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<PersonList />, document.getElementById('app'));
+});
