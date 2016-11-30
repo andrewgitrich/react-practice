0dbdd4c3488342123360629f3a4f012186b1fd29
andrew <andrew_rich@hotmail.com>
2016-11-30 13:46:38 -0500

    components

---
+++ js/components/person-list.js
@@ -0,0 +1,17 @@
+// js/components/person-list.js
+var Person = require('./person');
+
+var PersonList = function() {
+    var people = [];
+    for (var i=0; i<5; i++) {
+        people.push(<Person />);
+    }
+    return (
+        <div className="person-list">
+            {people}
+        </div>
+    );
+};
+
+
+module.exports = PersonList;
